cc815d55e563cef9ba6318c63e292a5ee5de0f87
Sambit Kumar Dalai <76642695+sambitkumardalai@users.noreply.github.com>
2022-04-09 14:51:27 +0530

    interpollation and event binding

--- word-generator/src/app/app.component.ts
+++ word-generator/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import arrayOfWords from '../utils/words';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'word-generator';
+  limit = 10;
+  words = '';
+  handleSlideChange(newLimit: number) {
+    this.limit = newLimit;
+  }
+  generate() {
+    this.words = arrayOfWords.slice(0, this.limit).join(' ');
+  }
 }
