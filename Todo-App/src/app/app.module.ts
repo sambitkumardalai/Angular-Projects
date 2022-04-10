82db0457ffd6c120460697c24f01d4962defb7bc
Sambit Kumar Dalai <76642695+sambitkumardalai@users.noreply.github.com>
2022-04-10 13:46:02 +0530

    angular template driven form

--- Todo-App/src/app/app.module.ts
+++ Todo-App/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 import { HeaderComponent } from './layout/header/header.component';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { HeaderComponent } from './layout/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Todo-App/src/app/components/todo-form/todo-form.component.ts
+++ Todo-App/src/app/components/todo-form/todo-form.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from './../../model/Todo';
+import { v4 as uuidv4 } from 'uuid';
+import { TodoService } from 'src/app/service/todo.service';
 
 @Component({
   selector: 'app-todo-form',
   templateUrl: './todo-form.component.html',
-  styleUrls: ['./todo-form.component.css']
+  styleUrls: ['./todo-form.component.css'],
 })
 export class TodoFormComponent implements OnInit {
-
-  constructor() { }
+  todoTitle: string = '';
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
+    //
   }
-
+  handleAdd() {
+    const newTodo: Todo = {
+      id: uuidv4(),
+      title: this.todoTitle,
+      isComplete: false,
+      date: new Date(),
+    };
+    this.todoService.addTodo(newTodo);
+    this.todoTitle = '';
+  }
 }
