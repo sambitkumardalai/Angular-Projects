1b646b914fb48db63bc9dfda309ce9aaae18b17c
Sambit Kumar Dalai <76642695+sambitkumardalai@users.noreply.github.com>
2022-06-07 07:33:40 +0530

    HostBinding

---
+++ directives-start/src/app/basic-highlight/host-binding.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from "@angular/core";
+
+@Directive({
+  selector: "[appHostBinding]",
+})
+export class HostBindingDirective {
+  @HostBinding("style.backgroundColor") backgroundColor: string = "cyan";
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+  @HostListener("mouseenter") mouseover(eventData: Event) {
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      "background-color",
+      "yellow"
+    );
+  }
+  @HostListener("mouseleave") mouseleave(eventData: Event) {
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      "background-color",
+      "transparent"
+    );
+  }
+}
