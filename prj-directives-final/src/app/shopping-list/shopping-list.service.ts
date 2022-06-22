f804d445dbb525ffe8502f655d58b06d66725d8b
Sambit Kumar Dalai <76642695+sambitkumardalai@users.noreply.github.com>
2022-06-22 07:07:10 +0530

    cross component communication through service

--- prj-directives-final/src/app/shopping-list/shopping-list.service.ts
+++ prj-directives-final/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ShoppingListService {
-
-  constructor() { }
+  ingredientChnaged = new EventEmitter<Ingredient[]>();
+  constructor() {}
+  private ingredients: Ingredient[] = [
+    new Ingredient("Apples", 5),
+    new Ingredient("Tomatoes", 10),
+  ];
+  getIngredients() {
+    return this.ingredients;
+  }
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientChnaged.emit(this.ingredients.slice());
+  }
 }
