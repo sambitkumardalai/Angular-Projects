34d5b866b38df0284369cb1e1f5e1b8f8ab76088
Sambit Kumar Dalai <76642695+sambitkumardalai@users.noreply.github.com>
2022-07-20 08:19:31 +0530

    query parameter and fragment in router navigate

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -10,7 +10,11 @@ export class HomeComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit() {}
-  onLoadServers() {
-    this.router.navigate(["/servers"]);
+  onLoadServers(id: number) {
+    this.router.navigate(["/servers", id, "edit"], 
+    {
+      queryParams: { allowEdit: "1" },
+      fragment:'loading'
+    });
   }
 }
