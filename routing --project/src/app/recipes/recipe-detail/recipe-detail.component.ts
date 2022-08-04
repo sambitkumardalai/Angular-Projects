c3f09f21dee1667c88dc76ae15d1b03c4b56428b
Sambit Kumar Dalai <76642695+sambitkumardalai@users.noreply.github.com>
2022-08-04 08:21:49 +0530

    passing dynamic parameters to links

--- routing --project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ routing --project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
 
-import { Recipe } from '../recipe.model';
-import { RecipeService } from '../recipe.service';
+import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  selector: "app-recipe-detail",
+  templateUrl: "./recipe-detail.component.html",
+  styleUrls: ["./recipe-detail.component.css"],
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
-
-  constructor(private recipeService: RecipeService) { }
+  recipe: any;
+  id: number;
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params["id"];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    });
   }
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
-
 }
