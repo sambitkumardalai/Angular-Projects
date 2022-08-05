aa8c52340c88046ed8736bb59ca2f635e7af94d8
Sambit Kumar Dalai <76642695+sambitkumardalai@users.noreply.github.com>
2022-08-05 08:33:43 +0530

    custom observable

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Observable, Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  customInternalObservableSubscription: Subscription;
+  constructor() {}
 
   ngOnInit() {
+    let count = 0;
+    const customIntervalObservable = new Observable<number>((observer) => {
+      setInterval(() => {
+        observer.next(count++);
+      }, 1000);
+    });
+    this.customInternalObservableSubscription =
+      customIntervalObservable.subscribe((data) => {
+        console.log(data);
+      });
   }
 
+  ngOnDestroy() {
+    this.customInternalObservableSubscription.unsubscribe();
+  }
 }
