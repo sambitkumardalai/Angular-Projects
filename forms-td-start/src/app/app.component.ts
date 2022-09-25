6349bc383a0cf4eaf0e6edb75f06b19119e26bf0
Sambit Kumar Dalai <76642695+sambitkumardalai@users.noreply.github.com>
2022-09-25 15:46:17 +0530

    reset template driven form

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -43,5 +43,7 @@ export class AppComponent {
     this.user.secretQuestion = this.signupForm.value.secret;
     this.user.answer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.username;
+
+    this.signupForm.reset();
   }
 }
