9631451d166f9ae93de54ea287c6ee99cf990bd4
Sambit Kumar Dalai <76642695+sambitkumardalai@users.noreply.github.com>
2022-09-25 15:44:46 +0530

    template driven form submit

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -11,10 +11,37 @@ export class AppComponent {
   defaultQuestion = "pet";
   answer = "";
   genders = ["male", "female"];
+
+  user = {
+    username: "",
+    email: "",
+    secretQuestion: "",
+    answer: "",
+    gender: "",
+  };
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = "Superuser";
+    // this.signupForm.setValue({
+    //   username: suggestedName,
+    //   email: "",
+    //   secret: "pet",
+    //   questionAnswer: "",
+    //   gender: "male",
+    // });
+
+    this.signupForm.form.patchValue({
+      username: suggestedName,
+    });
   }
   onSubmit() {
+    this.submitted = true;
     console.log(this.signupForm);
+    this.user.username = this.signupForm.value.username;
+    this.user.email = this.signupForm.value.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.username;
   }
 }
