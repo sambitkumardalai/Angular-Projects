8e9b06c5ce25f2da59db47ad63619d8baac72c76
Sambit <sambitskd3@gmail.com>
2022-10-18 21:01:08 +0530

    Loading the Shopping List Items into the Form

--- prj-obs-01-finished/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ prj-obs-01-finished/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  OnDestroy,
+} from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Subscription } from "rxjs";
 
 import { Ingredient } from "../../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list.service";
@@ -8,14 +15,36 @@ import { ShoppingListService } from "../shopping-list.service";
   templateUrl: "./shopping-edit.component.html",
   styleUrls: ["./shopping-edit.component.css"],
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild("f", { static: false }) slForm: NgForm;
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+  editedItem: Ingredient;
   constructor(private slService: ShoppingListService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.subscription = this.slService.startEditing.subscribe(
+      (index: number) => {
+        this.editedItemIndex = index;
+        this.editMode = true;
+        this.editedItem = this.slService.getIngredient(index);
+        console.log(this.editedItem);
+
+        this.slForm.setValue({
+          name: this.editedItem?.name,
+          amount: this.editedItem?.amount,
+        });
+      }
+    );
+  }
 
   onAddItem(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     this.slService.addIngredient(newIngredient);
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
--- prj-obs-01-finished/src/app/shopping-list/shopping-list.service.ts
+++ prj-obs-01-finished/src/app/shopping-list/shopping-list.service.ts
@@ -1,16 +1,20 @@
-import { Subject } from 'rxjs';
+import { Subject } from "rxjs";
 
-import { Ingredient } from '../shared/ingredient.model';
+import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
+  startEditing = new Subject<number>();
   private ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
+    new Ingredient("Apples", 5),
+    new Ingredient("Tomatoes", 10),
   ];
 
   getIngredients() {
     return this.ingredients.slice();
+  }
+  getIngredient(index: number) {
+    return this.ingredients[index];
   }
 
   addIngredient(ingredient: Ingredient) {
