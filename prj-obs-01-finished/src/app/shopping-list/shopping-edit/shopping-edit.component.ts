28b145d54da4a2e034809227033c65c55f20814a
Sambit <sambitskd3@gmail.com>
2022-10-31 21:40:05 +0530

    delete item

--- prj-obs-01-finished/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ prj-obs-01-finished/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -57,4 +57,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.slForm.reset();
     this.editMode = false;
   }
+  onDelete() {
+    this.slService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+  }
 }
