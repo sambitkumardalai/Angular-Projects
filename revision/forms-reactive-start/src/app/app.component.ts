e393ef8ab916faa75f221d33810a5ff7259e6e8b
Sambit Kumar Dalai <76642695+sambitkumardalai@users.noreply.github.com>
2023-01-22 07:37:55 +0530

    Reactive form: nested formgroup

--- revision/forms-reactive-start/src/app/app.component.ts
+++ revision/forms-reactive-start/src/app/app.component.ts
@@ -11,13 +11,15 @@ export class AppComponent implements OnInit {
   signUpForm: FormGroup;
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
-      username: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
+      userData: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+
       gender: new FormControl('male'),
     });
   }
   onSubmit() {
-    console.log(this.signUpForm.get('username'));
     console.log(this.signUpForm);
   }
 }
