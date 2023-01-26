d01edefde5b1de1ae9c4b1c26c02ff36504e6ee9
Sambit Kumar Dalai <76642695+sambitkumardalai@users.noreply.github.com>
2023-01-26 15:29:37 +0530

    delete item

--- prj-obs-01-finished/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ prj-obs-01-finished/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -42,4 +42,12 @@ export class ShoppingEditComponent implements OnInit {
     this.editMode = false;
     form.reset();
   }
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
+  }
+  onDelete() {
+    this.slService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+  }
 }
