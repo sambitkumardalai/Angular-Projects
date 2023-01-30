3db2f6142a04bb8a025da71e978542a18f7ff85d
Sambit Kumar Dalai <76642695+sambitkumardalai@users.noreply.github.com>
2023-01-30 21:58:48 +0530

    post request

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
@@ -15,7 +15,13 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http.post(
+      `https://ng-complete-guide-30d33-default-rtdb.firebaseio.com/posts.json`,
+      postData
+    ).subscribe(responseData=>{
+      console.log(responseData);
+      
+    })
   }
 
   onFetchPosts() {
