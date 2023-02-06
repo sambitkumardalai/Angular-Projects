0c9cbfcc5e7fdb48c6064d4d1bb6407b26b37bf6
Sambit Kumar Dalai <76642695+sambitkumardalai@users.noreply.github.com>
2023-02-06 22:36:20 +0530

    Firebase authentication

--- auth-01-auth-component/src/app/auth/auth.component.ts
+++ auth-01-auth-component/src/app/auth/auth.component.ts
@@ -1,18 +1,34 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.service";
 
 @Component({
   selector: "app-auth",
   templateUrl: "./auth.component.html",
 })
 export class AuthComponent {
+  constructor(private authService: AuthService) {}
   isLoginMode = true;
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
-  onSubmit(form:NgForm) {
-    console.log(form.value);
-    
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    if (this.isLoginMode) {
+      // 
+    } else {
+      this.authService.signUp(form.value.email, form.value.password).subscribe(
+        (resData) => {
+          console.log(resData);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+
     form.reset();
   }
 }
