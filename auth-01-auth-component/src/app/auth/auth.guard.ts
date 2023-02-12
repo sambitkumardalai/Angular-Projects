c925762f0444ceb2ce53ab7f59aaad3613389639
Sambit Kumar Dalai <76642695+sambitkumardalai@users.noreply.github.com>
2023-02-12 19:31:44 +0530

    take operator

--- auth-01-auth-component/src/app/auth/auth.guard.ts
+++ auth-01-auth-component/src/app/auth/auth.guard.ts
@@ -8,7 +8,7 @@ import {
 } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
-import { map } from "rxjs/operators";
+import { map, take } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
@@ -24,6 +24,7 @@ export class AuthGuard implements CanActivate {
     | boolean
     | UrlTree {
     return this.authService.user.pipe(
+      take(1),
       map((user) => {
         if (!!user) return true;
         return this.router.createUrlTree(["/auth"]);
